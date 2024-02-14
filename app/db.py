291c8bb5bc0773ee3029a045373adef4c47472e5
vladsaveliev <vladislav.savelyev@populationgenomics.org.au>
2024-02-14 15:57:29 +0100

    Buffer visits in memory and in csv before writing to database

--- app/db.py
+++ app/db.py
@@ -1,24 +1,29 @@
 """Functions to interact with the database."""
+import os
 
 import datetime
-from os import getenv
 
 from sqlmodel import create_engine, Field, select, Session, SQLModel
 
-sql_url = getenv("DATABASE_URL")
+sql_url = os.getenv("DATABASE_URL")
+assert sql_url is not None, sql_url
 engine = create_engine(sql_url)
 
 
-class Visit(SQLModel, table=True):  # type: ignore # mypy doesn't like this, not sure why
-    """Table to record raw individual visits to the version endpoint."""
+class Visits(SQLModel, table=True):  # type: ignore # mypy doesn't like this, not sure why
+    """
+    Table to record per-minute visit summaries
+    """
 
     id: int | None = Field(default=None, primary_key=True)
+    start: datetime.datetime | None = None
+    end: datetime.datetime | None = None
+    count: int = 0
     version_multiqc: str | None = None
     version_python: str | None = None
     operating_system: str | None = None
     installation_method: str | None = None
     ci_environment: str | None = None
-    called_at: datetime.datetime | None = Field(default_factory=datetime.datetime.utcnow, nullable=False)
 
 
 def create_db_and_tables() -> None:
@@ -26,27 +31,27 @@ def create_db_and_tables() -> None:
     SQLModel.metadata.create_all(engine)
 
 
-def add_visit(visit: Visit) -> None:
-    """Add a visit to the database."""
-    with Session(engine) as session:
-        session.add(visit)
-        session.commit()
+# def add_visit(visit: Visits) -> None:
+#     """Add a visit to the database."""
+#     with Session(engine) as session:
+#         session.add(visit)
+#         session.commit()
 
 
 def get_visits(
     start: datetime.datetime | None = None,
     end: datetime.datetime | None = None,
     limit: int | None = None,
-) -> list[Visit]:
-    """Return list of raw visits from the DB."""
+) -> list[Visits]:
+    """Return list of per-minute visit summary from the DB."""
     with Session(engine) as session:
-        statement = select(Visit)
+        statement = select(Visits)
         if start:
             # Ignore type because Visit.called_at can be None for default value
-            statement.where(Visit.called_at > start)  # type: ignore
+            statement.where(Visits.start >= start)  # type: ignore
         if end:
-            statement.where(Visit.called_at < end)  # type: ignore
+            statement.where(Visits.end <= end)  # type: ignore
         if limit:
             statement.limit(limit)
-        statement.order_by(Visit.called_at.desc())  # type: ignore
+        statement.order_by(Visits.start.desc())  # type: ignore
         return session.exec(statement).all()
